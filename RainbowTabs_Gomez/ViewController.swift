469db0c5d9e0275a6d29f3a5bdd3c1de6df39118
Byron Gomez Jr <byrongomezjr@protonmail.com>
2023-04-13 12:49:26 -0400

    added all five view controllers with respective colors.

--- RainbowTabs_Gomez/ViewController.swift
+++ RainbowTabs_Gomez/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        tabBarItem.badgeValue = nil
     }
 
 
